96116a685ab1a2687484eaab087768a9389db9b0
wipdev-tech <wipdev.tech@gmail.com>
2023-12-01 14:39:11 +0200

    Day 1 - Part 1 refactor
    Instead of reading the whole file by character, I used the bufio scanner
    to iterate over each line. This should make it easier for part two of
    the puzzle.

--- d01/d01.go
+++ d01/d01.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"strconv"
@@ -10,19 +11,31 @@ const newline = byte(10)
 
 func main() {
 	var total int
-	f, _ := os.ReadFile("./d01/d01input.txt")
+	file, _ := os.Open("./d01/d01input.txt")
+	defer file.Close()
 
-	digits := []int{}
-	for _, char := range f {
-		if char == newline {
-			d1 := digits[0]
-			d2 := digits[len(digits)-1]
+	// numbers := []string{
+	// 	"one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+	// }
 
-			total += d1*10 + d2
-			digits = []int{}
-		} else if d, err := strconv.Atoi(string(char)); err == nil {
-			digits = append(digits, d)
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+
+		digits := []int{}
+		for _, c := range line {
+			if d, err := strconv.Atoi(string(c)); err == nil {
+				digits = append(digits, d)
+			}
 		}
+
+		d1 := digits[0]
+		d2 := digits[len(digits)-1]
+
+		total += d1*10 + d2
+		digits = []int{}
 	}
 
 	fmt.Println(total)
