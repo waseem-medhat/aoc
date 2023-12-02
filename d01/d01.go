1f8d67378244d55527c2cf0cdb2ee8a75a401a9c
wipdev-tech <wipdev.tech@gmail.com>
2023-12-02 12:41:10 +0200

    Linting/formatting cleanup in Day 1 solution

--- d01/d01.go
+++ d01/d01.go
@@ -8,8 +8,6 @@ import (
 	"strings"
 )
 
-const newline = byte(10)
-
 func main() {
 	var total int
 	file, _ := os.Open("./d01/d01input.txt")
@@ -17,7 +15,7 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
-    for scanner.Scan() {
+	for scanner.Scan() {
 		line := scanner.Text()
 
 		digits := []int{}
@@ -25,16 +23,15 @@ func main() {
 		for i, char := range line {
 			if d, err := strconv.Atoi(string(char)); err == nil {
 				digits = append(digits, d)
-            } else if d := findNumber(line, i); d > 0 {
+			} else if d := findNumber(line, i); d > 0 {
 				digits = append(digits, d)
-            }
+			}
 		}
 
 		d1 := digits[0]
 		d2 := digits[len(digits)-1]
 
 		total += d1*10 + d2
-		digits = []int{}
 	}
 
 	fmt.Println(total)
@@ -45,11 +42,11 @@ func findNumber(line string, idx int) int {
 		"one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
 	}
 
-    for i, num := range numbers {
-        if strings.Index(line[idx:], num) == 0 {
-            return i+1
-        }
-    }
+	for i, num := range numbers {
+		if strings.Index(line[idx:], num) == 0 {
+			return i + 1
+		}
+	}
 
-    return -1
+	return -1
 }
