f25f212e1c6ebd0d3eef4296d471b69820d02833
wipdev-tech <wipdev.tech@gmail.com>
2023-12-03 17:23:48 +0200

    Day 3 - Part 1

--- d03/d03.go
+++ d03/d03.go
@@ -1,203 +1,144 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 	"strconv"
 )
 
-type Coord struct {
-	num string
-	i   int
-	j   int
-}
+const lineWidth = 140
+
+const zero = byte('0')
+const nine = byte('9')
+const dot = byte('.')
+const newLine = byte('\n')
+const asterisk = byte('*')
 
 func main() {
-	f, _ := os.Open("d03/d03input.txt")
-	defer f.Close()
+	fPre, _ := os.ReadFile("d03/d03input.txt")
+	f := []byte{}
+	for _, b := range fPre {
+		if b != newLine {
+			f = append(f, b)
+		}
+	}
 
-	nums, lines := getPartCoords(f)
-	partNumSum := checkAdjCoords(nums, lines)
+	num := []byte{}
+	isPartNum := false
+	partNumSum := 0
+
+	for i, b := range f {
+		if isDigit(b) {
+			num = append(num, b)
+
+			// start of a number, check its left (and diagonal left)
+			if len(num) == 1 && checkLeft(f, i) {
+				isPartNum = true
+			}
+
+			// middle of number, check its top and bottom
+			if checkTopBottom(f, i) {
+				isPartNum = true
+			}
+
+		} else if len(num) > 0 {
+			// we're at the right of a number
+			if checkRight(f, i-1) {
+				isPartNum = true
+			}
+
+			if isPartNum {
+				numInt, _ := strconv.Atoi(string(num))
+				partNumSum += numInt
+			}
+
+			num = []byte{}
+			isPartNum = false
+		}
+	}
 
 	fmt.Println(partNumSum)
 }
 
-// getPartCoords takes a *os.File (returned from os.Open()), extracts the part
-// numbers and returns a map from part number to its coordinates [row, col] in
-// the file. It also returns the file lines which will be used for lookup in
-// the later step.
-func getPartCoords(f *os.File) ([]Coord, []string) {
-	coords := []Coord{}
-	lines := []string{}
+// checkTopBottom takes a file (byte slice) and an index and checks if any of
+// characters on the top and bottom of the ith character (if any) is a symbol,
+// returning a bool
+func checkTopBottom(f []byte, i int) bool {
+	// default values are dots to evaluate to false in the last comparison
+	tChar := dot
+	bChar := dot
 
-	scanner := bufio.NewScanner(f)
-	for i := 0; scanner.Scan(); i++ {
-		line := scanner.Text()
-		lines = append(lines, line)
-
-		var newNum []rune
-		for j, char := range line {
-			if isDigit(char) {
-				newNum = append(newNum, char)
-			} else if len(newNum) > 0 {
-				// coords[string(newNum)] = [2]int{i, j - len(newNum)}
-				coords = append(coords, Coord{
-					num: string(newNum),
-					i:   i,
-					j:   j - len(newNum),
-				})
-				newNum = []rune{}
-			}
-		}
+	// top
+	if i > lineWidth {
+		tChar = f[i-lineWidth]
 	}
 
-	return coords, lines
-}
-
-// checkAdjCoords takes partCoords and a slice of lines (returned from
-// getPartCoords()) and returns true if the number is adjacent to a symbol in
-// any direction or even diagonally, and false otherwise.
-func checkAdjCoords(partCoords []Coord, lines []string) int {
-	partNumSum := 0
-
-outer:
-	for _, coord := range partCoords {
-		cLine := coord.i
-		cStart := coord.j
-		cEnd := cStart + len(coord.num) - 1
-
-		fmt.Println(coord.num, cLine, cStart)
-
-		var adjCoords [][2]int
-		var i int
-		var j int
-
-		// left, same line
-		if cStart > 0 {
-			i = cLine
-			j = cStart - 1
-			if checkChar(lines, i, j) {
-				// fmt.Printf("%c\n", lines[i][j])
-				numInt, _ := strconv.Atoi(coord.num)
-				partNumSum += numInt
-				continue
-			}
-			adjCoords = append(adjCoords, [2]int{i, j})
-		}
-
-		// upper left
-		if cStart > 0 && cLine > 0 {
-			i = cLine - 1
-			j = cStart - 1
-			if checkChar(lines, i, j) {
-				// fmt.Printf("%c\n", lines[i][j])
-				numInt, _ := strconv.Atoi(coord.num)
-                fmt.Println(numInt)
-				partNumSum += numInt
-				continue
-			}
-			adjCoords = append(adjCoords, [2]int{i, j})
-		}
-
-		// lower left
-		if cStart > 0 && cLine < len(lines)-1 {
-			i = cLine + 1
-			j = cStart - 1
-			if checkChar(lines, i, j) {
-				// fmt.Printf("%c\n", lines[i][j])
-				numInt, _ := strconv.Atoi(coord.num)
-                fmt.Println(numInt)
-				partNumSum += numInt
-				continue
-			}
-			adjCoords = append(adjCoords, [2]int{i, j})
-		}
-
-		// right, same line
-		if cEnd < len(lines[cLine])-1 {
-			i = cLine
-			j = cEnd + 1
-			if checkChar(lines, i, j) {
-				// fmt.Printf("%c\n", lines[i][j])
-				numInt, _ := strconv.Atoi(coord.num)
-                fmt.Println(numInt)
-				partNumSum += numInt
-				continue
-			}
-			adjCoords = append(adjCoords, [2]int{i, j})
-		}
-
-		// upper right
-		if cEnd < len(lines[cLine])-1 && cLine > 0 {
-			i = cLine - 1
-			j = cEnd + 1
-			if checkChar(lines, i, j) {
-				// fmt.Printf("%c\n", lines[i][j])
-				numInt, _ := strconv.Atoi(coord.num)
-                fmt.Println(numInt)
-				partNumSum += numInt
-				continue
-			}
-			adjCoords = append(adjCoords, [2]int{i, j})
-		}
-
-		// lower right
-		if cEnd < len(lines[cLine])-1 && cLine < len(lines)-1 {
-			i = cLine + 1
-			j = cEnd + 1
-			if checkChar(lines, i, j) {
-				// fmt.Printf("%c\n", lines[i][j])
-				numInt, _ := strconv.Atoi(coord.num)
-                fmt.Println(numInt)
-				partNumSum += numInt
-				continue
-			}
-			adjCoords = append(adjCoords, [2]int{i, j})
-		}
-
-		for j := cStart; j < cStart+len(coord.num); j++ {
-			// upper
-			if cLine > 0 {
-				i = cLine - 1
-				if checkChar(lines, i, j) {
-					// fmt.Printf("%c\n", lines[i][j])
-					numInt, _ := strconv.Atoi(coord.num)
-                    fmt.Println(numInt)
-					partNumSum += numInt
-					continue outer
-				}
-				adjCoords = append(adjCoords, [2]int{i, j})
-			}
-
-			// lower
-			if cLine < len(lines)-1 {
-				i = cLine + 1
-				if checkChar(lines, i, j) {
-					// fmt.Printf("%c\n", lines[i][j])
-					numInt, _ := strconv.Atoi(coord.num)
-                    fmt.Println(numInt)
-					partNumSum += numInt
-					continue outer
-				}
-				adjCoords = append(adjCoords, [2]int{i, j})
-			}
-		}
-		// fmt.Println(adjCoords)
+	// bottom left
+	if (i + lineWidth) < (len(f) - 1) {
+		bChar = f[i+lineWidth]
 	}
 
-	return partNumSum
+	return isSymbol(tChar) || isSymbol(bChar)
 }
 
-// checkCoord is a helper function for checkAdjCoords it takes a line slice and
-// checks if the jth character in the ith line is a symbol, returning a bool
-func checkChar(lines []string, i int, j int) bool {
-	c := rune(lines[i][j])
-	return !isDigit(c) && c != '.'
+// checkLeft takes a file (byte slice) and an index and checks if any of
+// characters on the left of the ith character (including top left and bottom
+// left if any) is a symbol, returning a bool
+func checkLeft(f []byte, i int) bool {
+	// default values are dots to evaluate to false in the last comparison
+	lChar := dot
+	tlChar := dot
+	blChar := dot
+
+	// left
+	if i > 0 {
+		lChar = f[i-1]
+	}
+
+	// top left
+	if i > lineWidth {
+		tlChar = f[i-lineWidth-1]
+	}
+
+	// bottom left
+	if (i + lineWidth) < (len(f) - 1) {
+		blChar = f[i+lineWidth-1]
+	}
+
+	return isSymbol(lChar) || isSymbol(tlChar) || isSymbol(blChar)
 }
 
-// isDigit is a simple helper that checks if a certain character (rune) is a
-// digit
-func isDigit(r rune) bool {
-	return r >= '0' && r <= '9'
+// checkRight takes a file (byte slice) and an index and checks if any of
+// characters on the right of the ith character (including top right and bottom
+// right if any) is a symbol, returning a bool
+func checkRight(f []byte, i int) bool {
+	// default values are dots to evaluate to false in the last comparison
+	rChar := dot
+	trChar := dot
+	brChar := dot
+
+	// right
+	if i < len(f)-1 {
+		rChar = f[i+1]
+	}
+
+	// top right
+	if i > lineWidth {
+		trChar = f[i-lineWidth+1]
+	}
+
+	// bottom right
+	if (i + lineWidth) < (len(f) - 1) {
+		brChar = f[i+lineWidth+1]
+	}
+
+	return isSymbol(rChar) || isSymbol(trChar) || isSymbol(brChar)
+}
+
+func isDigit(b byte) bool {
+	return b >= zero && b <= nine
+}
+
+func isSymbol(b byte) bool {
+	return !(isDigit(b) || b == dot)
 }
