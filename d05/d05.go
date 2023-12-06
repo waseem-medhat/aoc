6c5d19394deae71a594984dfae908ff3cd27d08b
wipdev-tech <wipdev.tech@gmail.com>
2023-12-06 16:32:22 +0200

    Day 5 - Part 2: func abstraction for concurrency

--- d05/d05.go
+++ d05/d05.go
@@ -14,75 +14,16 @@ func main() {
 	// f, _ := os.Open("d05/d05test.txt")
 	f, _ := os.Open("d05/d05input.txt")
 
-	var nums []int
-	maps := map[string][][3]int{}
-
-	newMap := [][3]int{}
-	newMapName := ""
-
-	scanner := bufio.NewScanner(f)
-	scanning := true
-	for lineNum := 0; scanning; lineNum++ {
-		scanning = scanner.Scan()
-		line := scanner.Text()
-
-		if lineNum == 0 {
-			nums = getSeeds(line)
-			continue
-		}
-
-		if lineNum == 1 {
-			continue
-		}
-
-		if strings.HasSuffix(line, "map:") {
-			newMapName = strings.Fields(line)[0]
-			newMap = [][3]int{}
-			continue
-		}
-
-		if line == "" || !scanning {
-			maps[newMapName] = newMap
-			continue
-		}
-
-		newMap = append(newMap, parseLine(line))
-	}
-
-	mapSeq := []string{
-		"seed-to-soil",
-		"soil-to-fertilizer",
-		"fertilizer-to-water",
-		"water-to-light",
-		"light-to-temperature",
-		"temperature-to-humidity",
-		"humidity-to-location",
-	}
-
+	nums, maps := parseAlmanac(f)
 	minLocation := math.Inf(1)
+
 	for seedNumIdx := 0; seedNumIdx < len(nums)-1; seedNumIdx += 2 {
 		firstNum := nums[seedNumIdx]
 		lastNum := firstNum + nums[seedNumIdx+1]
 
 		for num := firstNum; num < lastNum; num++ {
-			fmt.Println("Checking seed num: ", num)
 
-			// number updating
-            updatedNum := num
-		mapLoop:
-			for _, mapName := range mapSeq {
-				for _, row := range maps[mapName] {
-					dstStart := row[0]
-					srcStart := row[1]
-					length := row[2]
-					diff := dstStart - srcStart
-
-					if updatedNum >= srcStart && updatedNum < srcStart+length {
-						updatedNum += diff
-						continue mapLoop
-					}
-				}
-			}
+            updatedNum := lookupNum(num, &maps)
 
 			// number finished updating: check location
 			if float64(updatedNum) < minLocation {
@@ -91,7 +32,7 @@ func main() {
 		}
 	}
 
-    fmt.Println(minLocation)
+	fmt.Println(minLocation)
 }
 
 func getSeeds(line string) (seeds []int) {
@@ -127,4 +68,74 @@ func parseLine(line string) [3]int {
 	}
 
 	return [3]int{dstStart, srcStart, length}
+}
+
+func parseAlmanac(f *os.File) ([]int, map[string][][3]int) {
+	nums := []int{}
+	maps := map[string][][3]int{}
+	newMap := [][3]int{}
+	newMapName := ""
+
+	scanner := bufio.NewScanner(f)
+	scanning := true
+	for lineNum := 0; scanning; lineNum++ {
+		scanning = scanner.Scan()
+		line := scanner.Text()
+
+		if lineNum == 0 {
+			nums = getSeeds(line)
+			continue
+		}
+
+		if lineNum == 1 {
+			continue
+		}
+
+		if strings.HasSuffix(line, "map:") {
+			newMapName = strings.Fields(line)[0]
+			newMap = [][3]int{}
+			continue
+		}
+
+		if line == "" || !scanning {
+			maps[newMapName] = newMap
+			continue
+		}
+
+		newMap = append(newMap, parseLine(line))
+	}
+
+	return nums, maps
+}
+
+func lookupNum(num int, maps *map[string][][3]int) int {
+    fmt.Println("Checking seed num: ", num)
+
+	// number updating
+	mapSeq := []string{
+		"seed-to-soil",
+		"soil-to-fertilizer",
+		"fertilizer-to-water",
+		"water-to-light",
+		"light-to-temperature",
+		"temperature-to-humidity",
+		"humidity-to-location",
+	}
+
+mapLoop:
+	for _, mapName := range mapSeq {
+		for _, row := range (*maps)[mapName] {
+			dstStart := row[0]
+			srcStart := row[1]
+			length := row[2]
+			diff := dstStart - srcStart
+
+			if num >= srcStart && num < srcStart+length {
+				num += diff
+				continue mapLoop
+			}
+		}
+	}
+
+	return num
 }
