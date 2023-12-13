3100b22dae8068fb04a1d0ae23c5f6f1dabfc3e6
wipdev-tech <wipdev.tech@gmail.com>
2023-12-13 17:14:16 +0200

    Day 5 - Part 2: DONE!

--- d05/d05.go
+++ d05/d05.go
@@ -15,28 +15,33 @@ type numRange struct {
 }
 
 func main() {
-	seeds, rulesets := parseAlmanac("d05/d05test.txt")
-	// seeds, rulesets := parseAlmanac("d05/d05input.txt")
+	// seeds, rulesets := parseAlmanac("d05/d05test.txt")
+	seeds, rulesets := parseAlmanac("d05/d05input.txt")
 
 	ranges := []numRange{}
 	for i := 0; i < len(seeds)-1; i += 2 {
 		ranges = append(ranges, numRange{seeds[i], seeds[i] + seeds[i+1] - 1})
 	}
 
-	calcRanges := []numRange{ranges[0]}
-	for _, rs := range rulesets {
-		nRanges := len(calcRanges)
-		for j := 0; j < nRanges; j++ {
-			result := applyRuleset(calcRanges[0], rs)
-			calcRanges = calcRanges[1:]
-			calcRanges = append(calcRanges, result...)
-		}
+	allRanges := []numRange{}
+	for _, rng := range ranges {
+		calcRanges := []numRange{rng}
+		for _, rs := range rulesets {
+			nRanges := len(calcRanges)
+			for j := 0; j < nRanges; j++ {
+				result := applyRuleset(calcRanges[0], rs)
+				calcRanges = calcRanges[1:]
+				calcRanges = append(calcRanges, result...)
+			}
 
-		fmt.Println(calcRanges)
+			fmt.Println(calcRanges)
+		}
+		allRanges = append(allRanges, calcRanges...)
 	}
+	fmt.Println(allRanges)
 
 	var min int
-	for i, r := range calcRanges {
+	for i, r := range allRanges {
 		if i == 0 || r.start < min {
 			min = r.start
 		}
