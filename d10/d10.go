1b065e3ea3fa4e1eabcf84f595bfe3a9b125a4ae
wipdev-tech <wipdev.tech@gmail.com>
2024-02-05 16:10:12 +0200

    Day 10 - Initial traversal algorithm

--- d10/d10.go
+++ d10/d10.go
@@ -4,12 +4,102 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
-func main() {
-	maze, start := parseMaze("d10/test1.txt")
-	fmt.Println(strings.Join(maze, "\n"))
-	lookup(maze, start, 0)
+type Direction int
+
+const (
+	N Direction = iota
+	W
+	S
+	E
+	Stop
+)
+
+type Link int
+
+const (
+	NS Link = iota // |
+	EW             // -
+	NE             // L
+	NW             // J
+	SW             // 7
+	SE             // F
+)
+
+var pipes = map[rune]func([2]int, Direction) ([2]int, Direction){
+	'|': func(point [2]int, dir Direction) ([2]int, Direction) {
+		i := point[0]
+		j := point[1]
+		if dir == S {
+			return [2]int{i + 1, j}, S
+		}
+		if dir == N {
+			return [2]int{i - 1, j}, N
+		}
+		return [2]int{-1, -1}, Stop
+	},
+
+	'-': func(point [2]int, dir Direction) ([2]int, Direction) {
+		i := point[0]
+		j := point[1]
+		if dir == E {
+			return [2]int{i, j + 1}, E
+		}
+		if dir == W {
+			return [2]int{i, j - 1}, W
+		}
+		return [2]int{-1, -1}, Stop
+	},
+
+	'L': func(point [2]int, dir Direction) ([2]int, Direction) {
+		i := point[0]
+		j := point[1]
+		if dir == S {
+			return [2]int{i, j + 1}, E
+		}
+		if dir == W {
+			return [2]int{i - 1, j}, N
+		}
+		return [2]int{-1, -1}, Stop
+	},
+
+	'J': func(point [2]int, dir Direction) ([2]int, Direction) {
+		i := point[0]
+		j := point[1]
+		if dir == S {
+			return [2]int{i, j - 1}, W
+		}
+		if dir == E {
+			return [2]int{i - 1, j}, N
+		}
+		return [2]int{-1, -1}, Stop
+	},
+
+	'7': func(point [2]int, dir Direction) ([2]int, Direction) {
+		i := point[0]
+		j := point[1]
+		if dir == N {
+			return [2]int{i, j - 1}, W
+		}
+		if dir == E {
+			return [2]int{i + 1, j}, S
+		}
+		return [2]int{-1, -1}, Stop
+	},
+
+	'F': func(point [2]int, dir Direction) ([2]int, Direction) {
+		i := point[0]
+		j := point[1]
+		if dir == N {
+			return [2]int{i, j + 1}, E
+		}
+		if dir == W {
+			return [2]int{i + 1, j}, S
+		}
+		return [2]int{-1, -1}, Stop
+	},
 }
 
 // func parseMaze reads the file and parses the maze
@@ -29,23 +119,23 @@ func parseMaze(path string) (lines []string, start [2]int) {
 	return lines, start
 }
 
-func lookup(maze []string, point [2]int, steps int) {
-	i := point[0]
-	j := point[1]
+func tryMove(a, b [2]int, dir Direction) {
+}
 
-	if i > 0 {
-		fmt.Printf("Up: %c\n", maze[i-1][j])
-	}
-
-	if j > 0 {
-		fmt.Printf("Left: %c\n", maze[i][j-1])
-	}
-
-	if i < len(maze)-1 {
-		fmt.Printf("Down: %c\n", maze[i+1][j])
-	}
-
-	if j < len(maze[i])-1 {
-		fmt.Printf("Right: %c\n", maze[i][j+1])
+func main() {
+	maze, start := parseMaze("d10/test1.txt")
+	fmt.Println(strings.Join(maze, "\n"))
+	fmt.Println(start)
+	point := [2]int{1, 2}
+	dir := E
+	for {
+		pipe := rune(maze[point[0]][point[1]])
+		pipeFn, ok := pipes[pipe]
+		if !ok {
+			return
+		}
+		point, dir = pipeFn(point, dir)
+		fmt.Printf("%c\n", maze[point[0]][point[1]])
+		time.Sleep(1 * time.Second)
 	}
 }
