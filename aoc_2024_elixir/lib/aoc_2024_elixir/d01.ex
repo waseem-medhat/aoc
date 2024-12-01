222ba140bbf3277de05db9bd17063b230d9e773c
waseem-medhat <wipdev.tech@gmail.com>
2024-12-01 13:15:32 +0200

    Clean up code for 2024 Elixir D01

--- aoc_2024_elixir/lib/aoc_2024_elixir/d01.ex
+++ aoc_2024_elixir/lib/aoc_2024_elixir/d01.ex
@@ -1,15 +1,22 @@
 defmodule Aoc2024Elixir.D01 do
   def run() do
+    test_numbers = parse_numbers("lib/aoc_2024_elixir/d01/test1.txt")
+    input_numbers = parse_numbers("lib/aoc_2024_elixir/d01/input.txt")
+
     IO.puts("PART1")
-    solve_part_1("lib/aoc_2024_elixir/d01/test1.txt") |> IO.inspect(label: "test1")
-    solve_part_1("lib/aoc_2024_elixir/d01/input.txt") |> IO.inspect(label: "input")
+    solve_part_1(test_numbers) |> IO.inspect(label: "test1")
+    solve_part_1(input_numbers) |> IO.inspect(label: "input")
 
     IO.puts("\nPART2")
-    solve_part_2("lib/aoc_2024_elixir/d01/test1.txt") |> IO.inspect(label: "test1")
-    solve_part_2("lib/aoc_2024_elixir/d01/input.txt") |> IO.inspect(label: "input")
+    solve_part_2(test_numbers) |> IO.inspect(label: "test1")
+    solve_part_2(input_numbers) |> IO.inspect(label: "input")
+
     :ok
   end
 
+  @doc """
+  Parses the number lists from the given file path.
+  """
   def parse_numbers(path) do
     File.read!(path)
     |> String.split("\n", trim: true)
@@ -22,8 +29,11 @@ defmodule Aoc2024Elixir.D01 do
     end)
   end
 
-  def solve_part_1(path) do
-    {list_1, list_2} = parse_numbers(path)
+  @doc """
+  Calculates the score for part 1 by sorting the lists and calculating the
+  absolute distance between each pair.
+  """
+  def solve_part_1({list_1, list_2}) do
     list_1 = Enum.sort(list_1)
     list_2 = Enum.sort(list_2)
 
@@ -31,23 +41,35 @@ defmodule Aoc2024Elixir.D01 do
     |> Enum.sum()
   end
 
-  def solve_part_2(path) do
-    {list_1, list_2} = parse_numbers(path)
+  @doc """
+  Calculates the score for part 2 by building a similarity map (see
+  `build_similarity_map`). This was done to memoize the results as there are
+  many duplicates in list_1.
+  """
+  def solve_part_2({list_1, list_2}) do
+    build_similarity_map(list_1, list_2)
+    |> Enum.reduce(0, fn {_, %{freq: f, score: s}}, acc -> acc + f * s end)
+  end
 
-    similarity_map =
-      Enum.reduce(list_1, %{}, fn num, acc ->
-        case Map.has_key?(acc, num) do
-          true ->
-            acc
+  @doc """
+  Builds the similarity map with the following structure:
 
-          false ->
-            n_duplicates = list_2 |> Enum.filter(&(&1 == num)) |> length()
-            Map.put(acc, num, num * n_duplicates)
-        end
-      end)
+  - Each key is a number from list_1
+  - Each value is a map with:
+    + How many occurrences of that number in list 2
+    + The similarity score
+  """
+  def build_similarity_map(list_1, list_2) do
+    Enum.reduce(list_1, %{}, fn num, acc ->
+      case Map.has_key?(acc, num) do
+        true ->
+          Map.update!(acc, num, fn %{freq: f, score: s} -> %{freq: f + 1, score: s} end)
 
-    list_1
-    |> Enum.map(fn num -> Map.get(similarity_map, num) end)
-    |> Enum.sum()
+        false ->
+          n_duplicates = list_2 |> Enum.filter(&(&1 == num)) |> length()
+          score = num * n_duplicates
+          Map.put(acc, num, %{freq: 1, score: score})
+      end
+    end)
   end
 end
