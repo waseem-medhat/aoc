0cd225cf59f6d79569c538a4f013973509c01fef
waseem-medhat <wipdev.tech@gmail.com>
2024-12-03 13:34:38 +0200

    Solved 2024 Elixir D03 Part 2

--- aoc_2024_elixir/lib/aoc_2024_elixir/d03.ex
+++ aoc_2024_elixir/lib/aoc_2024_elixir/d03.ex
@@ -1,15 +1,20 @@
 defmodule Aoc2024Elixir.D03 do
   def run() do
-    test_instructions = File.read!("lib/aoc_2024_elixir/d03/test1.txt")
+    test1_instructions = File.read!("lib/aoc_2024_elixir/d03/test1.txt")
+    test2_instructions = File.read!("lib/aoc_2024_elixir/d03/test2.txt")
     input_instructions = File.read!("lib/aoc_2024_elixir/d03/input.txt")
 
-    IO.puts("PART 1")
-    solve(test_instructions) |> IO.inspect(label: "test1")
-    solve(input_instructions) |> IO.inspect(label: "input")
+    test1_parsed = parse(test1_instructions)
+    test2_parsed = parse(test2_instructions)
+    input_parsed = parse(input_instructions)
 
-    # IO.puts("\nPART 2")
-    # solve_part_2(test_reports) |> IO.inspect(label: "test1")
-    # solve_part_2(input_reports) |> IO.inspect(label: "input")
+    IO.puts("PART 1")
+    test1_parsed |> caluclate_part_1() |> IO.inspect(label: "test1")
+    input_parsed |> caluclate_part_1() |> IO.inspect(label: "input")
+
+    IO.puts("\nPART 2")
+    test2_parsed |> caluclate_part_2() |> IO.inspect(label: "test2")
+    input_parsed |> caluclate_part_2() |> IO.inspect(label: "input")
 
     :ok
   end
@@ -20,48 +25,66 @@ defmodule Aoc2024Elixir.D03 do
   defguard is_digit?(char) when char >= 48 and char <= 57
 
   # start
-  def solve(instructions),
-    do: solve(instructions, [], [], 0)
+  def parse(instructions),
+    do: parse(instructions, [], [], [])
 
   # end
-  def solve("", _num_1, _num_2, acc),
-    do: acc
+  def parse("", _num_1, _num_2, parsed),
+    do: parsed
+
+  # do's
+  def parse(<<"do()", rest::binary>>, _num_1, _num_2, parsed),
+    do: parse(rest, [], [], ["do" | parsed])
+
+  # dont's
+  def parse(<<"don't()", rest::binary>>, _num_1, _num_2, parsed),
+    do: parse(rest, [], [], ["dont" | parsed])
 
   # valid opening with digit after it
-  def solve(<<"mul(", char::utf8, rest::binary>>, _num_1, _num_2, acc)
+  def parse(<<"mul(", char::utf8, rest::binary>>, _num_1, _num_2, parsed)
       when is_digit?(char) do
-    solve(rest, [char], [], acc)
+    parse(rest, [char], [], parsed)
   end
 
   # invalid opening cause no digit after it
-  def solve(<<"mul(", _char::utf8, rest::binary>>, _num_1, _num_2, acc),
-    do: solve(rest, [], [], acc)
+  def parse(<<"mul(", _char::utf8, rest::binary>>, _num_1, _num_2, parsed),
+    do: parse(rest, [], [], parsed)
 
   # we have a num 1 and found a digit
-  def solve(<<char::utf8, rest::binary>>, [_ | _] = num_1, [] = _num_2, acc)
+  def parse(<<char::utf8, rest::binary>>, [_ | _] = num_1, [] = _num_2, parsed)
       when is_digit?(char) do
-    solve(rest, [char | num_1], [], acc)
+    parse(rest, [char | num_1], [], parsed)
   end
 
   # comma
-  def solve(<<",", char::utf8, rest::binary>>, [_ | _] = num_1, [] = _num_2, acc)
+  def parse(<<",", char::utf8, rest::binary>>, [_ | _] = num_1, [] = _num_2, parsed)
       when is_digit?(char),
-      do: solve(rest, num_1, [char], acc)
+      do: parse(rest, num_1, [char], parsed)
 
   # we have a num 2 and found a digit
-  def solve(<<char::utf8, rest::binary>>, [_ | _] = num_1, [_ | _] = num_2, acc)
+  def parse(<<char::utf8, rest::binary>>, [_ | _] = num_1, [_ | _] = num_2, parsed)
       when is_digit?(char),
-      do: solve(rest, num_1, [char | num_2], acc)
+      do: parse(rest, num_1, [char | num_2], parsed)
 
   # closer
-  def solve(<<")", rest::binary>>, [_ | _] = num_1, [_ | _] = num_2, acc) do
+  def parse(<<")", rest::binary>>, [_ | _] = num_1, [_ | _] = num_2, parsed) do
     num_1 = num_1 |> Enum.reverse() |> to_string() |> String.to_integer()
     num_2 = num_2 |> Enum.reverse() |> to_string() |> String.to_integer()
 
-    solve(rest, [], [], acc + num_1 * num_2)
+    parse(rest, [], [], [num_1 * num_2 | parsed])
   end
 
   # anything else is invalid
-  def solve(<<_::utf8, rest::binary>>, _num_1, _num_2, acc),
-    do: solve(rest, [], [], acc)
+  def parse(<<_::utf8, rest::binary>>, _num_1, _num_2, parsed),
+    do: parse(rest, [], [], parsed)
+
+  def caluclate_part_1(parsed), do: parsed |> Enum.filter(&is_number/1) |> Enum.sum()
+
+  def caluclate_part_2(parsed), do: parsed |> Enum.reverse() |> calculate_part_2(0, :do)
+
+  def calculate_part_2([], acc, _), do: acc
+  def calculate_part_2(["dont" | tail], acc, _), do: calculate_part_2(tail, acc, :dont)
+  def calculate_part_2(["do" | tail], acc, _), do: calculate_part_2(tail, acc, :do)
+  def calculate_part_2([_ | tail], acc, :dont), do: calculate_part_2(tail, acc, :dont)
+  def calculate_part_2([num | tail], acc, :do), do: calculate_part_2(tail, acc + num, :do)
 end
