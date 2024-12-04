cfb910331d2fe059b57d638c1242193231a558fd
waseem-medhat <wipdev.tech@gmail.com>
2024-12-04 14:21:15 +0200

    Solved 2024 Elixir D04 Part 2

--- aoc_2024_elixir/lib/aoc_2024_elixir/d04.ex
+++ aoc_2024_elixir/lib/aoc_2024_elixir/d04.ex
@@ -8,15 +8,23 @@ defmodule Aoc2024Elixir.D04 do
     input_lines |> solve_part_1() |> IO.inspect(label: "input")
 
     IO.puts("\nPART 2")
-    # input_parsed |> calculate_part_2() |> IO.inspect(label: "input")
+    test1_lines |> solve_part_2() |> IO.inspect(label: "test1")
+    input_lines |> solve_part_2() |> IO.inspect(label: "input")
 
     :ok
   end
 
+  @doc """
+  Reads the file and splits lines.
+  """
   def parse_lines(path) do
     File.read!(path) |> String.split("\n", trim: true)
   end
 
+  @doc """
+  Finds all occurrences of "XMAS" in horizontal, vertical, or diagonal orders
+  (including reversed).
+  """
   def solve_part_1(lines) do
     chunks = lines |> Stream.chunk_every(4, 1, :discard)
 
@@ -28,6 +36,19 @@ defmodule Aoc2024Elixir.D04 do
     n_horizontal + n_vertical + n_diag_forward + n_diag_backward
   end
 
+  @doc """
+  Finds all occurrences of "MAS" in an X shape.
+  """
+  def solve_part_2(lines) do
+    lines
+    |> Stream.chunk_every(3, 1, :discard)
+    |> Stream.map(&find_x/1)
+    |> Enum.sum()
+  end
+
+  @doc """
+  Given a single line, finds occurrences of XMAS or its reverse.
+  """
   def find_horizontal(line) do
     n_hits = Regex.scan(~r/XMAS/, line) |> length()
     n_rev_hits = Regex.scan(~r/SAMX/, line) |> length()
@@ -35,10 +56,16 @@ defmodule Aoc2024Elixir.D04 do
     n_hits + n_rev_hits
   end
 
+  @doc """
+  Given 4 lines and an index, extract the vertical word at that index.
+  """
   def extract_vertical_word(lines, index) when length(lines) == 4 do
     lines |> Enum.map(fn line -> String.slice(line, index, 1) end)
   end
 
+  @doc """
+  Given 4 lines, finds vertical occurrences of XMAS or its reverse.
+  """
   def find_vertical(lines) when length(lines) == 4 do
     n_elem = lines |> Enum.at(0) |> String.length()
 
@@ -52,6 +79,12 @@ defmodule Aoc2024Elixir.D04 do
     end)
   end
 
+  @doc """
+  Given 4 lines, finds forward-diagonal occurrences if XMAS or its reverse.
+
+  It does so by shifting the lines so that the diagonals become vertically
+  aligned, and then it applies `find_vertical/1` on the aligned lines.
+  """
   def find_diag_forward(lines) when length(lines) == 4 do
     lines
     |> Enum.with_index(fn line, i ->
@@ -60,11 +93,38 @@ defmodule Aoc2024Elixir.D04 do
     |> find_vertical()
   end
 
+  @doc """
+  Given 4 lines, finds backward-diagonal occurrences if XMAS or its reverse.
+
+  It does so by shifting the lines so that the diagonals become vertically
+  aligned, and then it applies `find_vertical/1` on the aligned lines.
+  """
   def find_diag_backward(lines) when length(lines) == 4 do
     lines
     |> Enum.with_index(fn line, i ->
       String.slice(line, 3 - i, String.length(line) - i)
     end)
     |> find_vertical()
+  end
+
+  @doc """
+  Given 3 lines, finds all occurrences of MAS (or its reverse) in an X shape.
+  """
+  def find_x(lines) when length(lines) == 3 do
+    n_elem = lines |> Enum.at(0) |> String.length()
+
+    Range.new(0, n_elem - 3)
+    |> Enum.reduce(0, fn i, acc ->
+      substrings =
+        Enum.map(lines, fn line -> String.slice(line, i, 3) end)
+
+      case substrings do
+        [<<"M", _, "S">>, <<_, "A", _>>, <<"M", _, "S">>] -> acc + 1
+        [<<"M", _, "M">>, <<_, "A", _>>, <<"S", _, "S">>] -> acc + 1
+        [<<"S", _, "M">>, <<_, "A", _>>, <<"S", _, "M">>] -> acc + 1
+        [<<"S", _, "S">>, <<_, "A", _>>, <<"M", _, "M">>] -> acc + 1
+        _ -> acc
+      end
+    end)
   end
 end
