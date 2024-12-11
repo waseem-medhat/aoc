b9bbe14642daaa673e4c60576ac98b14af01f3e2
waseem-medhat <wipdev.tech@gmail.com>
2024-12-11 13:37:02 +0200

    Solved 2024 Elixir D10 Part 2

--- aoc_2024_elixir/lib/d10/solver.ex
+++ aoc_2024_elixir/lib/d10/solver.ex
@@ -5,18 +5,34 @@ defmodule D10.Solver do
     parse("lib/d10/test2.txt") |> solve_part_1() |> IO.inspect(label: "test2")
     parse("lib/d10/test3.txt") |> solve_part_1() |> IO.inspect(label: "test3")
     parse("lib/d10/input.txt") |> solve_part_1() |> IO.inspect(label: "input")
+
+    IO.puts("\nPART2")
+    parse("lib/d10/test4.txt") |> solve_part_2() |> IO.inspect(label: "test4")
+    parse("lib/d10/test1.txt") |> solve_part_2() |> IO.inspect(label: "test1")
+    parse("lib/d10/test5.txt") |> solve_part_2() |> IO.inspect(label: "test5")
+    parse("lib/d10/input.txt") |> solve_part_2() |> IO.inspect(label: "input")
+
+    :ok
   end
 
+  @doc """
+  Parses the file in the given path into a map of `{i, j} => num` with an extra
+  entry `:zero_locs` that stores the starting points in a list.
+  """
   def parse(path) do
-    File.read!(path) |> parse_map(0, 0, %{})
+    File.read!(path) |> parse_map(0, 0, %{zero_locs: []})
   end
+
+  @doc """
+  Recursively parses the given map string.
+  """
+  def parse_map(string, i, j, acc)
 
   def parse_map("\n", _, _, acc), do: acc
+
   def parse_map("\n" <> rest, i, _j, acc), do: parse_map(rest, i + 1, 0, acc)
 
-  def parse_map("0" <> rest, i, j, acc) do
-    zero_locs = Map.get(acc, :zero_locs, [])
-
+  def parse_map("0" <> rest, i, j, %{zero_locs: zero_locs} = acc) do
     acc =
       acc
       |> Map.put({i, j}, ?0)
@@ -30,13 +46,21 @@ defmodule D10.Solver do
     parse_map(rest, i, j + 1, acc)
   end
 
+  @doc """
+  Solves part 1 by counting the number of 9's reached from each starting point
+  and summing them.
+  """
   def solve_part_1(map) do
-    Enum.map(map.zero_locs, fn zero_loc -> walk(map, zero_loc, nil) end)
+    Enum.map(map.zero_locs, fn loc -> find_nines(map, loc, nil) end)
     |> Enum.map(&MapSet.size/1)
     |> Enum.sum()
   end
 
-  def walk(map, {i, j}, direction) do
+  @doc """
+  Returns a `MapSet` of the unique locations of 9's visited given a single
+  starting point.
+  """
+  def find_nines(map, {i, j}, direction) do
     current_num = Map.get(map, {i, j})
 
     case current_num do
@@ -44,19 +68,56 @@ defmodule D10.Solver do
         MapSet.new([{i, j}])
 
       _ ->
-        calc_next_points({i, j}, direction)
-        |> Enum.reduce(
-          MapSet.new(),
-          fn {next_direction, next_point}, acc ->
-            if Map.get(map, next_point) == current_num + 1,
-              do: MapSet.union(walk(map, next_point, next_direction), acc),
-              else: acc
+        next_points = get_next_points({i, j}, direction)
+
+        Enum.reduce(next_points, MapSet.new(), fn {next_direction, next_point}, acc ->
+          next_num = Map.get(map, next_point)
+
+          case next_num == current_num + 1 do
+            true -> MapSet.union(find_nines(map, next_point, next_direction), acc)
+            false -> acc
           end
-        )
+        end)
     end
   end
 
-  def calc_next_points({i, j}, direction) do
+  @doc """
+  Solves part 2 by counting the number of paths that lead to a 9 for each
+  starting point and summing them.
+  """
+  def solve_part_2(map) do
+    Enum.map(map.zero_locs, fn zero_loc -> calc_rating(map, zero_loc, nil) end)
+    |> Enum.sum()
+  end
+
+  @doc """
+  Counts the number of 9's for all traversals from the given starting point.
+  """
+  def calc_rating(map, {i, j}, direction) do
+    current_num = Map.get(map, {i, j})
+
+    case current_num do
+      ?9 ->
+        1
+
+      _ ->
+        next_points = get_next_points({i, j}, direction)
+
+        Enum.reduce(next_points, 0, fn {next_direction, next_point}, acc ->
+          next_num = Map.get(map, next_point)
+
+          case next_num == current_num + 1 do
+            true -> calc_rating(map, next_point, next_direction) + acc
+            false -> acc
+          end
+        end)
+    end
+  end
+
+  @doc """
+  Utility: calculates next points to traverse, excluding the backwards one.
+  """
+  def get_next_points({i, j}, direction) do
     opposite_direction =
       case direction do
         :up -> :down
