5b7fad10588111f6aa6bd9d3be24aaf6ecff69ba
waseem-medhat <wipdev.tech@gmail.com>
2024-12-11 16:27:53 +0200

    Solved 2024 Elixir D09 Part 2

--- aoc_2024_elixir/lib/d09/solver.ex
+++ aoc_2024_elixir/lib/d09/solver.ex
@@ -7,8 +7,8 @@ defmodule D09.Solver do
 
     IO.puts("\nPART2")
 
-    # parse_map("lib/d08/test1.txt") |> solve_part_2() |> IO.inspect(label: "test1")
-    # parse_map("lib/d08/input.txt") |> solve_part_2() |> IO.inspect(label: "input")
+    parse("lib/d09/test1.txt") |> solve_part_2() |> IO.inspect(label: "test1")
+    parse("lib/d09/input.txt") |> solve_part_2() |> IO.inspect(label: "input")
 
     :ok
   end
@@ -19,17 +19,9 @@ defmodule D09.Solver do
   """
   def parse(path), do: File.read!(path) |> unpack()
 
-  def solve_part_1(line) do
-    line
-    |> rearrange()
-    |> calc_checksum()
-  end
-
   @doc """
   Unpacks the given line into a list of files and spaces, where a file is
   represented by its ID as an integer, and spaces are `nil`.
-
-  This 1-arity variant initializes the 4-arity one with the needed values.
   """
   def unpack(line), do: unpack(line, [], 0, :file)
 
@@ -41,19 +33,30 @@ defmodule D09.Solver do
   """
   def unpack(line, acc, current_file_id, type)
 
-  # nothing more to unpack, reverse the list back in order 
   def unpack("\n", acc, _, _), do: Enum.reverse(acc)
 
-  # unpacking a file, repeat its id into a list and prepend it to `acc`
   def unpack(<<char, rest::binary>>, acc, file_id, :file) do
-    acc = repeat(file_id, digit_to_int(char)) ++ acc
+    acc = [%{id: file_id, size: digit_to_int(char)} | acc]
     unpack(rest, acc, file_id + 1, :space)
   end
 
-  # unpacking a space, repeat `nil` into a list and prepend it to `acc`
   def unpack(<<char, rest::binary>>, acc, file_id, :space) do
-    acc = repeat(nil, digit_to_int(char)) ++ acc
+    acc = [%{id: nil, size: digit_to_int(char)} | acc]
     unpack(rest, acc, file_id, :file)
+  end
+
+  def solve_part_1(block_list) do
+    block_list
+    |> blocks_to_vals()
+    |> rearrange_vals()
+    |> calc_checksum()
+  end
+
+  def solve_part_2(block_list) do
+    block_list
+    |> rearrange_blocks()
+    |> blocks_to_vals()
+    |> calc_checksum()
   end
 
   @doc """
@@ -64,11 +67,11 @@ defmodule D09.Solver do
   dropping extra values (that should have been spaces), the list is reversed
   back into order.
   """
-  def rearrange(line) do
+  def rearrange_vals(line) do
     vals_to_rearrange = extract_vals_to_rearrange(line)
     n_spaces = Enum.count(line, fn val -> val == nil end)
 
-    rearrange(line, vals_to_rearrange, [])
+    rearrange_vals(line, vals_to_rearrange, [])
     |> Enum.drop(n_spaces)
     |> Enum.reverse()
   end
@@ -78,34 +81,90 @@ defmodule D09.Solver do
 
   It uses cons for accumulation, so the output will be in reverse order.
   """
-  def rearrange(line, vals_to_rearrange, acc)
+  def rearrange_vals(line, vals_to_rearrange, acc)
 
-  # no more values to rearrange, prepend the rest of the line
-  def rearrange(line, [], acc),
+  def rearrange_vals(line, [], acc),
     do: Enum.reverse(line) ++ acc
 
-  # found a space, add value from the value list to `acc`
-  def rearrange([nil | rest], [val | rest_to_rearrange], acc),
-    do: rearrange(rest, rest_to_rearrange, [val | acc])
+  def rearrange_vals([nil | rest], [val | rest_to_rearrange], acc),
+    do: rearrange_vals(rest, rest_to_rearrange, [val | acc])
 
-  # found a number, add value from the original line to `acc`
-  def rearrange([val | rest], vals_to_rearange, acc),
-    do: rearrange(rest, vals_to_rearange, [val | acc])
+  def rearrange_vals([val | rest], vals_to_rearange, acc),
+    do: rearrange_vals(rest, vals_to_rearange, [val | acc])
+
+  @doc """
+  Recursively rearranges blocks.
+  """
+  def rearrange_blocks(block_list),
+    do: rearrange_blocks(block_list, [], MapSet.new([]))
+
+  defp rearrange_blocks([], acc, _shifted),
+    do: Enum.reverse(acc)
+
+  defp rearrange_blocks([%{id: nil, size: space_size} = space_block | rest], acc, shifted) do
+    to_shift =
+      rest
+      |> Enum.reverse()
+      |> Enum.find(fn block ->
+        space? = is_nil(block.id)
+        already_shifted? = MapSet.member?(shifted, block)
+        too_large? = block.size > space_size
+
+        not (space? or already_shifted? or too_large?)
+      end)
+
+    case to_shift do
+      nil ->
+        acc = [space_block | acc]
+        rearrange_blocks(rest, acc, shifted)
+
+      file_block ->
+        shifted = MapSet.put(shifted, file_block)
+
+        case merge_blocks(space_block, file_block) do
+          [block] ->
+            acc = [block | acc]
+            rearrange_blocks(rest, acc, shifted)
+
+          [block, space_block] ->
+            acc = [block | acc]
+            rearrange_blocks([space_block | rest], acc, shifted)
+        end
+    end
+  end
+
+  defp rearrange_blocks([block | rest], acc, shifted) do
+    acc =
+      case MapSet.member?(shifted, block) do
+        true -> [%{id: nil, size: block.size} | acc]
+        false -> [block | acc]
+      end
+
+    rearrange_blocks(rest, acc, shifted)
+  end
 
   @doc """
   Calculates the checksum of a line by multiplying each value by its index and
   summing the multiplication results.
   """
-  def calc_checksum(line) do
-    line
+  def calc_checksum(vals) do
+    vals
     |> Enum.with_index()
-    |> Enum.reduce_while(0, fn
-      {nil, _}, acc -> {:halt, acc}
-      {val, i}, acc -> {:cont, val * i + acc}
+    |> Enum.reduce(0, fn
+      {nil, _}, acc -> acc
+      {val, i}, acc -> val * i + acc
     end)
   end
 
-  # utilities
+  def merge_blocks(%{id: nil, size: space_size}, %{id: file_id, size: file_size}) do
+    cond do
+      space_size > file_size ->
+        [%{id: file_id, size: file_size}, %{id: nil, size: space_size - file_size}]
+
+      space_size == file_size ->
+        [%{id: file_id, size: file_size}]
+    end
+  end
 
   @doc """
   Utility: converts the digit ASCII value to an integer, assuming only 0-9
@@ -114,8 +173,21 @@ defmodule D09.Solver do
   def digit_to_int(char), do: char - ?0
 
   @doc """
+  Utility: spreads blocks in the given list as values, repeating the IDs
+  according to the sizes.
+  """
+  def blocks_to_vals(block_list), do: blocks_to_vals(block_list, [])
+
+  defp blocks_to_vals([], acc), do: Enum.reverse(acc)
+
+  defp blocks_to_vals([%{id: id, size: size} | rest], acc) do
+    acc = List.duplicate(id, size) ++ acc
+    blocks_to_vals(rest, acc)
+  end
+
+  @doc """
   Utility: for a given line (list), extracts values that are going be to
-  shifted to spaces when calling `rearrange/3`.
+  shifted to spaces when calling `rearrange_vals/3`.
 
   This is done by figuring out how many spaces in the line and subtracting from
   those the spaces that are already "in place" (spaces whose initial position
@@ -139,10 +211,4 @@ defmodule D09.Solver do
 
     vals_to_rearrange
   end
-
-  @doc """
-  Utility: returns a list with `val` repeated `n` times.
-  """
-  def repeat(_, 0), do: []
-  def repeat(val, n), do: Enum.map(Range.new(1, n), fn _ -> val end)
 end
